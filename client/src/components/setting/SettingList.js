e3da9289a68a64210048fb70bfee6587bcda1f99
JaiRaga <raga.j.santhosh@gmail.com>
2021-06-17 18:23:20 +0530

    btns to change navigation inside setting page

---
+++ client/src/components/setting/SettingList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+const SettingList = ({ name }) => {
+	return <Button fullWidth>{name}</Button>
+}
+
+export default SettingList
