012150b5d9b467b525f46a3617f1e305f0d9cbf9
JaiRaga <raga.j.santhosh@gmail.com>
2021-06-17 18:14:41 +0530

    refactored code

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,19 +1,8 @@
-import React, { useEffect } from 'react'
-import io from 'socket.io-client'
+import React from 'react'
 import { Grid } from '@material-ui/core'
 import ChatItem from './ChatItem'
 
-let socket
-const SERVERURL = 'http://localhost:9008'
-
 const Chat = () => {
-	useEffect(() => {
-		socket = io(SERVERURL)
-		socket.on('connection', () => {
-			console.log(socket.id)
-		})
-	}, [])
-
 	// const classes = useStyles()
 	return (
 		<Grid container>
