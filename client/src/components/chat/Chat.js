d58d3f9bce2a5380679ff96e18ee8329e443254b
JaiRaga <raga.j.santhosh@gmail.com>
2021-06-18 17:34:27 +0530

    passing user info of the users who the current user has had a chat history with

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
+import { useSelector } from 'react-redux'
 import ChatItem from './ChatItem'
 
 const Chat = () => {
 	// const classes = useStyles()
+	const chats = useSelector((state) => state.user.users)
+	const user = useSelector((state) => state.auth.user)
+
 	return (
 		<Grid container>
-			<ChatItem />
-			<ChatItem />
-			<ChatItem />
-			<ChatItem />
-			<ChatItem />
-			<ChatItem />
+			{chats.map(
+				(chat) =>
+					chat.username !== user.username && (
+						<ChatItem key={chat._id} chat={chat} />
+					)
+			)}
 		</Grid>
 	)
 }
