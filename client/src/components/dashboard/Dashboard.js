6a40c25ae782f7739e2d422232c72bba265c604a
JaiRaga <raga.j.santhosh@gmail.com>
2021-07-10 14:24:19 +0530

    added loading indicator/spinner

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, CircularProgress, makeStyles } from '@material-ui/core'
 import { useDispatch, useSelector } from 'react-redux'
 import TabChange from '../layout/TabChange'
 import { socket } from '../messages/Messages'
 import { addChats } from '../../redux/actions/chat'
 
+const useStyles = makeStyles((theme) => ({
+	loading: {
+		marginTop: 20,
+	},
+}))
+
 const Dashboard = () => {
 	const dispatch = useDispatch()
+	const classes = useStyles()
 	const user = useSelector((state) => state.auth.user)
 	let _id
 
@@ -22,9 +29,15 @@ const Dashboard = () => {
 
 	return (
 		<Grid container justify='center' alignItems='center'>
-			<Grid container item xs={12} md={9} lg={6}>
-				<TabChange />
-			</Grid>
+			{!user ? (
+				<div className={classes.loading}>
+					<CircularProgress />
+				</div>
+			) : (
+				<Grid container item xs={12} md={9} lg={6}>
+					<TabChange />
+				</Grid>
+			)}
 		</Grid>
 	)
 }
